c7272c4602ebbc98a286326ffd06cf8e268c9957
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-09-01 11:59:49 +0100

    Set port to Heroku deploy

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {User} = require('./models/user');
 const {ObjectID}  = require('mongodb');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -52,8 +53,8 @@ app.get('/todos/:id', (req,res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`started at port ${port}`);
 });
 
 module.exports = {app};
